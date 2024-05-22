1ff9b87eb7ae0181f6774e5a27c763f98942ecb8
Seva <137809136+Sevatoxin@users.noreply.github.com>
2024-05-22 23:15:49 +0200

    Added pseudocode for the calculator

---
+++ index.js
@@ -0,0 +1,29 @@
+// Pseudocode:
+// - Create a Calculator
+// - Create a function for every basic calculator evaluation
+//     -> ADD / SUBSTRACT / MULTIPLY / DIVIDE
+// - Create a Button that clears the calc screen
+// - Create an operator function that takes 2 numbers and the operator as arguments
+// - In HTML each number 0 to 10 and the operator each have a button as have the clear and result button
+// - Let the numbers be on screen when click in a number button
+// - Numbers appear always on the right to build a bigger number
+
+function add(num1, num2) {
+    return (num1 + num2);
+}
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
+
+function divide(num1, num2) {
+    return (num1 / num2);
+}
+
+function operate(num1, num2, operation) {
+    return;
+}
